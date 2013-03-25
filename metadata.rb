2096a6db88f5d78d7d134ac06c589e9db7e147eb
Eduardo Diaz <iamedu@Gmail.com>
2013-03-24 18:57:19 -0600

    Adding metadata

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+maintainer       "Eduardo Diaz"
+maintainer_email "iamedu@gmail.com"
+license          "Apache 2.0"
+description      "Installs Oracle XE."
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.1"
+
+recipe "oracle-xe", "Installs Oracle XE."
+
+supports "ubuntu"
+
